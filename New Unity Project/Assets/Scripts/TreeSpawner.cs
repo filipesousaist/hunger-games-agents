9675b0a9c1672c9a74d0180d493fb8a86b826766
JoaoGuerreiro <joao.guerreiro@eviso.it>
2021-04-21 21:00:41 +0100

    Moving Camera

--- New Unity Project/Assets/Scripts/TreeSpawner.cs
+++ New Unity Project/Assets/Scripts/TreeSpawner.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TreeSpawner : MonoBehaviour
 {
+    public int TREE_AMOUNT;
+    
+    public GameObject tree;
+    
+    private System.Random random = new System.Random();
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        for (int i = 0; i < TREE_AMOUNT; i++) {
+            GameObject newTree = Instantiate(tree);
+
+            newTree.transform.position = new Vector3(random.Next(-245, 245), 0f, random.Next(-245, 245));
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }

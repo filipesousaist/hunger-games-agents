91a7932904e478f31bcbb7f5e1411512f76d19f9
miquelroq <miguelroque99@tecnico.ulisboa.pt>
2021-05-27 02:59:29 +0100

    bdi wip

--- hunger-games/Assets/Scripts/Agents/Decision Modules/Belief.cs
+++ hunger-games/Assets/Scripts/Agents/Decision Modules/Belief.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
@@ -10,6 +11,9 @@ public class Belief
     private Dictionary<Vector3,BushData> bushes;
     private Dictionary<Vector3, ChestData> chests;
     private Tuple<AgentData, int>[] agentsData;
+    private HazardEffectData[] hazardsOrder;
+
+    private AgentData myData;
 
 
     public Belief()
@@ -23,13 +27,63 @@ public class Belief
         bushes = new Dictionary<Vector3, BushData>();
         chests = new Dictionary<Vector3, ChestData>();
         agentsData = new Tuple<AgentData, int>[Const.NUM_AGENTS];
-
+        hazardsOrder = new HazardEffectData[Const.NUM_REGIONS];
 
     }
 
     public void UpdateAgentsData(Tuple<AgentData, int> agentData)
     {
         agentsData[agentData.Item1.index - 1] = agentData;
+    }
+
+    public void UpdateMyData(AgentData agentData)
+    {
+        myData = agentData;
+    }
+
+    public void UpdateHazardsOrder(HazardEffectData[] order)
+    {
+        hazardsOrder = order;
+        if (hazardsOrder.Contains(null))
+        {
+            int filled = 0;
+            int missingIndex = 0;
+            HazardEffectData[] filledHazards = new HazardEffectData[hazardsOrder.Length];
+            
+            for (int i = 0; i < hazardsOrder.Length; i++)
+            {
+                HazardEffectData hazard = order[i];
+                
+                if (hazard == null)
+                {
+                    missingIndex = i;
+                }
+                else
+                {
+                    filledHazards[i] = hazard;
+                    filled++;
+                }
+            }
+            if (filled == Const.NUM_REGIONS-1)
+            {
+                hazardsOrder[missingIndex] = new HazardEffectData();
+                foreach (Hazard.Type hazardType in new Hazard.Type[]{Hazard.Type.FOG, Hazard.Type.FIRE,  Hazard.Type.RAIN,  Hazard.Type.RADIATION})
+                {
+                    if ((filledHazards.Count((hazardAux) => hazardAux.hazardType == hazardType)) == 1)
+                    {
+                        hazardsOrder[missingIndex].hazardType = hazardType;
+                    }
+                }
+
+                for (int i = 0; i < Const.NUM_REGIONS; i++)
+                {
+                    if ((filledHazards.Count((hazardAux) => hazardAux.region == i)) == 0)
+                    {
+                        hazardsOrder[missingIndex].region = i;
+                    }
+                }
+            }
+        }
     }
 
     public void UpdateMap(IEnumerable<EntityData> entities)

79ee3f3dd79936133422c0c04a6730beb3518876
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2021-05-28 18:02:54 +0100

    hyb

--- hunger-games/Assets/Scripts/Agents/Concrete Deciders/HybridDecider.cs
+++ hunger-games/Assets/Scripts/Agents/Concrete Deciders/HybridDecider.cs
@@ -1,26 +1,73 @@
 using System.Collections.Generic;
 using static Agent;
+using UnityEngine;
 
 public class HybridDecider : Decider
 {
     private ReactiveModule reactiveModule;
-    private BDIModule bDIModule;
+    private DecisionTheoreticalModule dTModule;
 
-    // 1º Reactive
-    // Se houver ação urgente, faz essa ação
-    // Senão vai ao BDI
-    // Se houver um plano já feito, faz a próxima ação do plano
-    // Senão tenta fazer um novo plano
+    private DecisionModule currentModule;
+
+    private int lastEnergy = Const.MAX_ENERGY;
+
+    private const float RANDOM_MODULE_CHANCE = 0.2f;
+
+    private Dictionary<DecisionModule, int> score;
+    private Dictionary<DecisionModule, DecisionModule> other;
 
     private void Awake()
     {
         reactiveModule = new ReactiveModule(this);
-        bDIModule = new BDIModule(this);
+        dTModule = new DecisionTheoreticalModule(this);
+        score = new Dictionary<DecisionModule, int>()
+        {
+            { reactiveModule, 0 },
+            { dTModule, 0 }
+        };
+        other = new Dictionary<DecisionModule, DecisionModule>()
+        {
+            { reactiveModule, dTModule },
+            { dTModule, reactiveModule }
+        };
+        currentModule = reactiveModule;
     }
 
     public override void Decide(Perception perception)
     {
+        score[currentModule] += GetScore(perception);
 
+        reactiveModule.Decide(perception);
+        if (!reactiveModule.isUrgent)
+        {
+            if (score[currentModule] < score[other[currentModule]]) // Change module if other seems better
+                currentModule = other[currentModule];
+
+            DecisionModule decisionModule = 
+                Random.Range(0, 1f) >= RANDOM_MODULE_CHANCE ? currentModule : other[currentModule];
+
+            if (decisionModule.Equals(dTModule))
+            {
+                Debug.Log("Using DT with score:");
+                dTModule.Decide(perception);
+            }
+            else
+            {
+                Debug.Log("Using Reactive with score:");
+            }
+            Debug.Log(score[decisionModule] " vs " score[otherModule[decisionModule]]);
+        }
+        Debug.Log("Using urgent Reactive");
+            
+    }
+
+    private int GetScore(Perception perception)
+    {
+        int diff = perception.myData.energy - lastEnergy;
+
+        return diff >  0 ? 1 :
+               diff == 0 ? 0 :
+                          -1;
     }
 
     public override string GetArchitectureName()
